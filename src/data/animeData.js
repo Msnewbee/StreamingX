3756646944d4548206c4bfe491f72eab09156a07
Msnewbee <bilariko2@gmail.com>
2025-03-12 02:04:28 +0700

    Create animeData.js

---
+++ src/data/animeData.js
@@ -0,0 +1,64 @@
+const animeData = [
+  {
+    id: 1,
+    title: "Solo Leveling",
+    image: "https://drive.google.com/thumbnail?id=1cImeMono1VBtfRHlYOxBMBfWRV-HLvIB",
+    genre: "Aksi, Fantasi Portal",
+    rating: "8.5/10",
+    synopsis:
+      "Mengisahkan Sung Jin-woo, seorang hunter terlemah yang mendapatkan kekuatan unik untuk 'level up' tanpa batas setelah selamat dari dungeon ganda yang mematikan.",
+    episodes: [
+      {
+        number: 1,
+        url: "https://mega.nz/embed/wVQESDiL#rWrzrxNp6Ip17-s-dICc2iaMr73T5Pic5Ep563F3vlY!"
+      },
+      {
+        number: 2,
+        url: "https://mega.nz/embed/gA5VnRBL#7RQII5KdmoV-Tj4OwaBOJ3ncg7q70Et7K86OE4WLPA0!"
+      },
+      {
+        number: 3,
+        url: "https://mega.nz/embed/9d43yZgA#JfeYXtCuhCNDbxejLHRDkeTdW3nRK1Xz5RC1sAowfL4!"
+      },
+      {
+        number: 4,
+        url: "https://mega.nz/embed/5UQCmIYR#4Lsgd_7srCe5I40zUoAqYruTYSkh6RumVAOocKoD5-g!"
+      },
+      {
+        number: 5,
+        url: "https://mega.nz/embed/sEwWjTwZ#MJwYHtqIN7hp_yXCqeIyqACGZtIOVZ-YrKeoGesmBpA!"
+      },
+      {
+        number: 6,
+        url: "https://mega.nz/embed/hYxzCLBQ#hfwORuSgBA7kw5_YDlqNQf-YPJhuCbc08jzzzRHQXgA!"
+      },
+      {
+        number: 7,
+        url: "https://mega.nz/embed/xYxjCCKK#IXwLYHqhopYbAH6VZ4ZVv-9RxuoE0OTBgB5VkZicwJA!"
+      },
+      {
+        number: 8,
+        url: "https://mega.nz/embed/cdAzCbzY#I5LwEY-QLsk6XkgJ4Fd_fWE403R_wZV6t-lOGb5-lQM!"
+      },
+      {
+        number: 9,
+        url: "https://mega.nz/embed/5VxjxJ7A#mOLObws05S43C2hxxoiCAJqkJs-O3ro1LIXdoMP9xeo!"
+      },
+      {
+        number: 10,
+        url: "https://mega.nz/embed/Mcp3jbJI#bwfSBck90h_u0X050bXrQF5k7KZVnNR4hY_1HN4EaNI!"
+      },
+      {
+        number: 11,
+        url: "https://mega.nz/embed/4N5mTQyZ#1P2XC3_DohVFY_d1wpR80Zf1U0ORhXwX7wG7298rSg0!"
+      },
+      {
+        number: 12,
+        url: "https://mega.nz/embed/BZpjALxA#9Bl_75xizjLGer-xbb9m8qMLbufl10ChJK9Y7JhFmwc!"
+      }
+    ]
+  }
+  // Tambahkan anime lain jika diperlukan
+];
+
+export default animeData;
