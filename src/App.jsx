e29ce58e71ec164f2ec24f519427a89bd3bad3e5
Msnewbee <bilariko2@gmail.com>
2025-03-12 23:40:03 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import AnimeList from "./components/AnimeList";
-import AnimeDetail from "./components/AnimeDetail";
-import animeData from "./data/animeData";
-import Home from "./components/Home"; // Tambahkan import Home
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <BrowserRouter> {//}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(() => {
